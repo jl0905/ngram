3950fe52f4ea7f384a33b88ac299023ede25f648
Jeffery Lin <jlin20@wm.edu>
2026-03-05 22:57:02 -0500

    somewhat working

--- ngram.py
+++ ngram.py
@@ -1,43 +1,59 @@
 # GenAI NGrams (assignment 1) Jeffery Lin
 
-####
+import json
 
-def readMethods():
-	line_number = 1
-	word_counts = {}
+
+def readMethods(filename, size):
+	line_number = 0
+	# word_counts = {}
 	methods = []
-	with open("methods.txt", "r") as f:
+	with open(filename, "r") as f:
 		for line in f:
-			if line_number == 15000: #COMMENT OUT 
-				break
-			if line_number % 2 == 0:
-				#IF THE LINE IS A METHOD WE NEED TO DO STUFF
-				methods.append(line.strip())
-				line_number += 1
-				continue
-
-			line = line[6:].strip()
-
-			if line in word_counts:
-				word_counts[line] += 1
+			if filename == "methods.txt":
+				if line_number == size * 2: #COMMENT OUT 
+					break
+				if line_number % 2 == 1:
+					#IF THE LINE IS A METHOD WE NEED TO DO STUFF
+					methods.append(line.strip().split(" "))
+					line_number += 1
+					continue
 			else:
-				word_counts[line] = 1
+				if line_number == size:
+					break
+				methods.append(line.strip().split(" "))
+			# line = line[6:].strip()
+
+			# if line in word_counts:
+			# 	word_counts[line] += 1
+			# else:
+			# 	word_counts[line] = 1
 			line_number += 1
 	return methods
 ####
 
 class NGram:
 	def __init__(self, N, tokenized_input):
+		self.tokenized_input = self.preprocessUnknownTokens(tokenized_input)
 		
 		self.n = N
 		self.ngram = self.constructNGram(tokenized_input, N)
 		self.backoff_ngrams = [self.constructNGram(tokenized_input, previousN) for previousN in range(N - 1, 0, -1)]
 
 		vocab = set()
-		for entry in tokenized_input:
-			for token in entry:
+		for method in tokenized_input:
+			for token in method:
 				vocab.add(token)
 		self.vocab = list(vocab)
+
+	def preprocessUnknownTokens(self, tokenized_input):
+		temp_unigram = self.constructNGram(tokenized_input, 1)
+		for i in range(len(tokenized_input)):
+			method = tokenized_input[i]
+			for j in range(len(method)):
+				token = method[j]
+				if temp_unigram[token] < 3: #IF LESS THAN 3 occurences we treat it as unknown
+					method[j] = "<UNK>"
+		return tokenized_input
 
 	def constructNGram(self, tokenized_input, n):
 		'''Returns an ngram where the keys are all tokens n-1 and before and values are
@@ -49,7 +65,7 @@ class NGram:
 		if n == 1:
 			ngram = {}
 			for method in tokenized_input:
-				for token in method.split(" "):
+				for token in method:#.split(" "):
 					if token in ngram.keys():
 						ngram[token] += 1
 					else:
@@ -58,10 +74,10 @@ class NGram:
 		ngram = {} # keys are tuple and value is dict
 		for method in tokenized_input:
 			tokens = ["<START>" for _ in range(n-1)]
-			tokens.extend(method.split(" "))
+			tokens.extend(method)#.split(" "))
 			tokens.append("<END>")
-			if tokens > self.n:
-				pass
+			# if tokens > self.n:
+			# 	pass
 			#print(tokens)
 			for i in range(len(tokens) - n + 1):
 				#print(tokens[i:i+n-1])
@@ -81,6 +97,9 @@ class NGram:
 
 	def getProbabilityOfToken(self, context, token):
 		'''precondition is context has to be a tuple (w_i-n ... w_i - 1) and token is w_i'''
+		# if token not encountered in vocab its oov
+		if token not in self.vocab:
+			token = "<UNK>"
 		if (self.n == 1):
 			return self.ngram[token] / sum(self.ngram.values())
 		if context in self.ngram.keys() and token in self.ngram[context].keys():
@@ -90,126 +109,87 @@ class NGram:
 			for i in range(len(self.backoff_ngrams)):#ngram in self.backoff_ngrams:
 				ngram = self.backoff_ngrams[i]
 				if (i == len(self.backoff_ngrams) - 1): # if n is one we have different key value pairs
+					# if token not in ngram:
+					# 	return 0
 					return penalty * ngram[token] / sum(ngram.values())
 				if context in ngram.keys() and token in ngram[context].keys():
 					return penalty * (ngram[context][token] / sum(ngram[context].values()))
 				else:
 					penalty = penalty * 0.8
 
-	#def evaluate(self, )
+	def evaluate(self, evaluation_set, write_to=""):
+		perplexity = 1
+		probabilities = []
+		n = self.n
+		epsilon = 1e-10  # Small number to prevent zero probabilities
+		for method in evaluation_set:
+			tokens = ["<START>" for _ in range(self.n-1)]
+			tokens.extend(method)#.split(" "))
+			tokens.append("<END>")
+			for i in range(len(tokens) - n + 1):
+				k = tuple(tokens[i:i+n-1])
+				v = tokens[i+n-1]
+				pred_probability = self.getProbabilityOfToken(k, v)
+				# Clamp probability to avoid zero
+				pred_probability = max(pred_probability, epsilon)
+				probabilities.append(pred_probability)
+				perplexity *= (1 / pred_probability)
+		perplexity = perplexity ** (1 / len(probabilities))
+		if write_to != "":
+			self.writeResults(write_to, evaluation_set, perplexity, probabilities)
 
+	def writeResults(self, filename, evaluation_set, perplexity, probabilities):
+		tokenized_methods = evaluation_set
+		token_i = 0
+		ID = 1
+		n = self.n
+		with open(filename, "w") as f:
+			tab = "	"
+			f.write("{\n")
+			f.write(tab + "\"testSet\": \"provided.txt\",\n")
+			f.write(tab + "\"contextWindow\": " + str(self.n) + ",\n")
+			f.write(tab + "\"perplexity\": " + str(perplexity) + ",\n")
+			f.write(tab + "\"data\": [\n")
+			#for tokenized_method in tokenized_methods:
+			for j in range(len(tokenized_methods)):
+				tokenized_method = tokenized_methods[j]
+				f.write(tab + tab + "{\n")
+				f.write(tab + tab + tab + "\"index\": \"" + "ID" + str(ID) + "\",\n")
+				ID += 1
+				f.write(tab + tab + tab + "\"tokenizedCode\": " + json.dumps("".join([mtd + " " for mtd in tokenized_method])[:-1]) + ",\n")
+				f.write(tab + tab + tab + "\"predictions\": [\n")
+				# f.write(tab + tab + tab + tab + "{\n")
+				# f.write(tab + tab + tab + tab + tab + "\"context\": [\"public\", \"void\"],\n")
+				# f.write(tab + tab + tab + tab + tab + "\"predToken\": \"run\",\n")
+				# f.write(tab + tab + tab + tab + tab + "\"predProbability\": 0.72,\n")
+				# f.write(tab + tab + tab + tab + tab + "\"groundTruth\": \"run\"\n")
+				# f.write(tab + tab + tab + tab + "},\n")
+				tokens = ["<START>" for _ in range(self.n-1)]
+				tokens.extend(tokenized_method)#.split(" "))
+				tokens.append("<END>")
+				for i in range(len(tokens) - n + 1):
+					k = tuple(tokens[i:i+n-1])
+					v = tokens[i+n-1]
+					f.write(tab + tab + tab + tab + "{\n")
+					f.write(tab + tab + tab + tab + tab + "\"context\": " + json.dumps(list(k)) + ",\n")
+					f.write(tab + tab + tab + tab + tab + "\"groundTruth\": " + json.dumps(v) + ",\n")
+					f.write(tab + tab + tab + tab + tab + "\"predProbability\": " + str(probabilities[token_i]) + "\n")
+					token_i += 1
+					if (i == len(tokens) - n):
+						f.write(tab + tab + tab + tab + "}\n")
+					else:
+						f.write(tab + tab + tab + tab + "},\n")
+				f.write(tab + tab + tab + "]\n")
+				if j == len(tokenized_methods) - 1:
+					f.write(tab + tab + "}\n")
+				else:
+					f.write(tab + tab + "},\n")
+			f.write(tab + "]\n")
+			f.write("}\n")
 
-# def pruneNGram(ngram, n):
-# 	to_delete = []
-# 	for context in ngram.keys():
-# 		for potential_values in ngram[context].keys():
-# 			if ngram[context][potential_values] < n:
-# 				to_delete.append(tuple([context, potential_values]))
-# 				#del ngram[context][potential_values]
-# 	for pair in to_delete:
-# 		del ngram[pair[0]][pair[1]]
-# 	return ngram
-
-	# def smoothingHelper(self, smoothed_list):
-	# 	ret = []
-	# 	for gram in smoothed_list:
-	# 		for word in self.vocab:
-	# 			new_gram = [thing for thing in gram]
-	# 			new_gram.append(word)
-	# 			ret.append(new_gram)
-	# 	return ret
-
-
-	# def smoothNGram(self):
-	# 	smoothed_list = [word for word in self.vocab]
-	# 	for _ in range(self.n):
-	# 		smoothed_list = self.smoothingHelper(smoothed_list)
-	# 	for gram in smoothed_list:
-	# 		key = tuple(gram[:-1])
-	# 		value = gram[-1]
-	# 		if key not in self.ngram.keys():
-	# 			self.ngram[key] = dict()
-	# 		if value not in self.ngram[key].keys():
-	# 			self.ngram[value] = 1
-	# 		else:
-	# 			self.ngram[value] += 1
-
-
-def generateNextToken(ngram, context, n):
-	curr = tuple(context[len(context)- n + 1:])
-	distribution = ngram[curr]
-	#distribution is like {"public": 0, "static": 100, "the": 3}
-	
-	# Convert counts to probabilities
-	total_count = sum(distribution.values())
-	if total_count == 0:
-		return None
-	
-	# Create weighted random choice
-	tokens = list(distribution.keys())
-	weights = list(distribution.values())
-	
-	# Normalize weights to probabilities
-	probabilities = [w / total_count for w in weights]
-	
-	# Randomly sample based on probabilities
-	import random
-	selected_token = random.choices(tokens, weights=probabilities, k=1)[0]
-	
-	return selected_token[0] if isinstance(selected_token, tuple) else selected_token
-
-quit()
-
-N = 3
-all_methods = readMethods()
+all_methods = readMethods("methods.txt", 35000)
+test_methods = readMethods("test.txt", 100)
 
 test = NGram(3, all_methods)
-#test.smoothNGram()
-print(test.ngram[('arr', '.')])
-
-def autoRegress(ngram):
-	starting_context = ["<START>" for _ in range(N - 1)]
-		#print(starting_context)
-
-	for i in range(15):
-		starting_context.append(generateNextToken(ngram.ngram, starting_context, N))
-		if starting_context[-1] == "<END>": break
-		#print(starting_context)
-
-	print(starting_context)
-
-autoRegress(test)
-
-def calculatePerplexity():
-	** (1/n)
-
-def writeResults(filename):
-	with open(filename+".json", "w") as f:
-		tab = "	"
-		def quote(s):
-			return "\"" + s + "\""
-		f.write("{\n")
-		f.write(tab + "\"testSet\": \"provided.txt\",\n")
-		f.write(tab + "\"perplexity\": 4.39,\n")
-		f.write(tab + "\"data\": [\n")
-		f.write(tab + tab + "{\n")
-		f.write(tab + tab + tab + "\"index\": \"ID1\",\n")
-		f.write(tab + tab + tab + "\"tokenizedCode\": \"public void run ( ) { }\",\n")
-		f.write(tab + tab + tab + "\"contextWindow\": 3,\n")
-		f.write(tab + tab + tab + "\"predictions\": [\n")
-		f.write(tab + tab + tab + tab + "{\n")
-		f.write(tab + tab + tab + tab + tab + "\"context\": [\"public\", \"void\"],\n")
-		f.write(tab + tab + tab + tab + tab + "\"predToken\": \"run\",\n")
-		f.write(tab + tab + tab + tab + tab + "\"predProbability\": 0.72,\n")
-		f.write(tab + tab + tab + tab + tab + "\"groundTruth\": \"run\"\n")
-		f.write(tab + tab + tab + tab + "},\n")
-		f.write(tab + tab + tab + tab + "{\n")
-		f.write(tab + tab + tab + tab + tab + "\"context\": [\"void\", \"run\"],\n")
-		f.write(tab + tab + tab + tab + tab + "\"predToken\": \"(\",\n")
-		f.write(tab + tab + tab + tab + tab + "\"predProbability\": 0.85,\n")
-		f.write(tab + tab + tab + tab + tab + "\"groundTruth\": \"(\"\n")
-		f.write(tab + tab + tab + tab + "}\n")
-		f.write(tab + tab + tab + "]\n")
-		f.write(tab + tab + "}\n")
-		f.write(tab + "]\n")
-		f.write("}\n")
+#print(test.ngram[tuple(["private"])])
+test.evaluate(test_methods, "results.json")
--- report.md
+++ report.md
@@ -2,4 +2,6 @@ ABSTRACT
 
 An N-Gram model is essentially a table of all possible permutations of N tokens (in other words all sentences, or grams) that are observed in the training set for the model. To utilize the NGram model, we access the conditional probability of the n-th token based on the previous n-1 tokens from our table. For the task of creating an NGram model, we attain a large mass of open source Java methods and we process these methods as training data for our model and also add back-off smoothing. The model is then evaluated twice; on a predetermined test set as well as a random test set from the training data. Evaluation is calculated through the perplexity via model performance on ground truth tokens in the testing set.   
 
-        We started the semester by being introduced to how we can mine public GitHub repositories for data to train our models on. 
+       
+
+We started the semester by being introduced to how we can mine public GitHub repositories for data to train our models on. By using 
